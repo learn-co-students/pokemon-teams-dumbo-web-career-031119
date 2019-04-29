0aed14cc057252e33e01034d5c151a6436ff766b
siberry <siberry226@gmail.com>
2019-04-29 15:38:46 -0400

    done

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -99,19 +99,3 @@ const removePokemon = (pokemonId) => {
   })
   .then(resp => resp.json())
 }
-
-const removePokemonFromPage = (pokemon) => {
-  debugger
-}
-
-//
-// <div class="card" data-id="1"><p>Prince</p>
-//   <button data-trainer-id="1">Add Pokemon</button>
-//   <ul>
-//     <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
-//     <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-//     <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-//     <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-//     <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
-//   </ul>
-// </div>
